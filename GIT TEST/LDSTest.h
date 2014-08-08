9c65d487637188c8fd3292ee375ef6574804b59f
Lewis Steelman <lewissteelman@Lewiss-2013-Mac-mini.local>
2014-08-08 18:01:29 -0500

    added a test class

---
+++ GIT TEST/LDSTest.h
@@ -0,0 +1,13 @@
+//
+//  LDSTest.h
+//  GIT TEST
+//
+//  Created by Lewis Steelman on 8/8/14.
+//  Copyright (c) 2014 Mine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LDSTest : NSObject
+
+@end
---
+++ GIT TEST/LDSTest.m
@@ -0,0 +1,13 @@
+//
+//  LDSTest.m
+//  GIT TEST
+//
+//  Created by Lewis Steelman on 8/8/14.
+//  Copyright (c) 2014 Mine. All rights reserved.
+//
+
+#import "LDSTest.h"
+
+@implementation LDSTest
+
+@end
